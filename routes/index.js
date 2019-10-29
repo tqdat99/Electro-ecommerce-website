bb2934e094c53a522f3eb3734e540ba9f417e6a3
tqdat99 <56955642+tqdat99@users.noreply.github.com>
2019-10-29 16:15:18 +0700

    Thay nội dung trang chủ

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/store', function(req, res, next) {
+    res.render('store');
 });
 
 module.exports = router;
