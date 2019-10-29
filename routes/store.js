87ee4587bbb63f0dd60b2126ec46155049f96f08
tqdat99 <56955642+tqdat99@users.noreply.github.com>
2019-10-29 17:23:46 +0700

    Thay dữ liệu trang cửa hàng

--- routes/store.js
+++ routes/store.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'Express' });
+    res.render('store');
 });
+
+// router.get('/product', function(req, res) {
+//     res.render('index');
+// });
 
 module.exports = router;
