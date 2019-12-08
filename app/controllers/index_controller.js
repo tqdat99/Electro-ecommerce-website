d08dd2ffa3b5ab5672bbd57095b3ce61270e4333
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2019-12-08 22:04:02 +0700

    update index_controller

--- app/controllers/index_controller.js
+++ app/controllers/index_controller.js
@@ -2,6 +2,6 @@ var laptops = require('../models/laptop')
 
 exports.home = function(req,res) {
 	laptops.getItems(function(items) {
-		res.render('home', { Items: items });
+		res.render('home', { Items: items, User: req.user  });
 	})
 }
