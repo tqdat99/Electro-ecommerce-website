f43f0d739256257fd44a20f3fe63f9792255c39d
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2019-12-09 19:27:09 +0700

    [Update related products] controller

--- app/controllers/store_controller.js
+++ app/controllers/store_controller.js
@@ -1,22 +1,32 @@
 var laptops = require('../models/laptop')
 
-exports.productlist = function(req,res) {
-	laptops.getProductList(req.params['type'], function(items) {
-		res.render('store', { Items: items , User: req.user });
-	})
-}
-
 exports.productdetail = function(req, res){
-	laptops.getProductDetail(req.params['id'], function(items){
-		if (items.rows.length > 0) {
-			res.render('product-details', { item: items.rows[0] , User: req.user });
+	laptops.getProductDetail(req.params['id'], function(item){
+		if (item.rows.length > 0) {
+			laptops.getRelatedItems(item.rows[0].loai, function(items) {
+				res.render('product-details', { item: item.rows[0] , User: req.user , Items: items});
+			})
 		}
 	})
 }
 
-exports.productlistBrand = function(req, res){
-	console.log(req.params);
-	laptops.getProductBrand(req.params['type'], req.params['brand'], function(items){
-		res.render('store', {Items: items , User: req.user});
+exports.productlist = function(req,res) {
+	laptops.getProductList(req.params['type'], function(items) {
+		res.render('store', { Items: items , Type: req.params['type'], Brand: 'undefined', User: req.user });
 	})
 }
+
+exports.productlistBrand = function(req, res){
+	laptops.getProductBrand(req.params['type'], req.params['brand'], function(items){
+		res.render('store', {Items: items , Type: req.params['type'], Brand: req.params['brand'], User: req.user});
+	})
+}
+
+exports.productlistOrder = function(req, res){
+	laptops.getProductOrder(req.params['type'], req.params['brand'], req.params['order'], function(items){
+		res.render('store', {Items: items , Type: req.params['type'], Brand: req.params['brand'], User: req.user}); // ????????
+	})
+}
+
+
+
