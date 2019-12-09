6b9b6cca438741b0866e36564cee8770ff056948
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2019-12-09 19:28:55 +0700

    [Update order by price] route

--- routes/store.js
+++ routes/store.js
@@ -3,6 +3,8 @@ var router = express.Router()
 
 var store = require('../app/controllers/store_controller')
 
+router.get('/product-list-:type-:brand-:order', store.productlistOrder)
+
 router.get('/product-list-:type-:brand', store.productlistBrand)
 
 router.get('/product-list-:type',store.productlist)
