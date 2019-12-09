b19f47550df321772675dc98eb815640a632baf6
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2019-12-09 17:05:46 +0700

    Update order by price

--- controllers/store.js
+++ controllers/store.js
@@ -2,13 +2,13 @@ var productModel = require('../models/product')
 
 module.exports.productListByType = function(req, res) {
     productModel.getProductListByType(req.params['type'], function(items) {
-        res.render('store', { Items: items });
+        res.render('store', { Items: items , Type: req.params['type'], Brand: 'undefined'});
     })
 }
 
 module.exports.productListByTypeAndBrand = function(req, res) {
     productModel.getProductListByTypeAndBrand(req.params['type'], req.params['brand'], function(items) {
-        res.render('store', { Items: items });
+        res.render('store', { Items: items , Type: req.params['type'], Brand: req.params['brand']});
     })
 }
 
@@ -19,3 +19,10 @@ module.exports.productDetailById = function(req, res) {
 		}
     })
 }
+
+module.exports.productlistOrder = function(req, res){
+	productModel.getProductOrder(req.params['type'], req.params['brand'], req.params['order'], function(items){
+		res.render('store', {Items: items , Type: req.params['type'], Brand: req.params['brand'], User: req.user}); // ????????
+	})
+}
+
