6c567607d5e8d81e465d38148126e54412c023b7
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2019-12-09 19:27:34 +0700

    Update model laptop

--- app/models/laptop.js
+++ app/models/laptop.js
@@ -1,44 +1,55 @@
 const { Pool } = require('pg')
 const pool = new Pool({
-    user: 'xljfmycrkdjihn',
-    database: 'learn3',
+    user: 'anhthu',
+    database: 'dbat',
     host: 'localhost',
     port: 5432
 });
 
 exports.getItems = function(callback) {
-    query = 'select * from "Laptop" limit 4';
+    query = 'select * from "products" limit 4';
     pool.query(query, function(err, result) {
         callback(result);
     });
 }
 
-exports.getProductList = function(type, callback) {
-    query = 'select * from \"' + type + '\"';
-    pool.query(query, function(err, result) {
-        callback(result);
-    })
-}
-
-exports.getProductDetail = function(id, callback){
-    queryLaptop = "select * from \"Laptop\" where id = '" + id + "'";
-    queryDienthoai = "select * from \"Dienthoai\" where id = '" + id + "'";
-    queryTivi = "select * from \"Tivi\" where id = '" + id + "'";
-    pool.query(queryLaptop, function(err, result) {
-        callback(result);
-    })
-    pool.query(queryDienthoai, function(err, result) {
-        callback(result);
-    })
-    pool.query(queryTivi, function(err, result) {
-        callback(result);
-    })
-}
-
-exports.getProductBrand = function(type, brand, callback){
-    query = 'select * from \"' + type + '\" where brand = \'' + brand + '\'';
+exports.getRelatedItems = function(type, callback) {
+    query = "select * from \"products\" where loai = '" + type + "' limit 4";
     console.log(query)
     pool.query(query, function(err, result) {
         callback(result);
     });
 }
+
+exports.getProductDetail = function(id, callback){
+    query = "select * from \"products\" where id = '" + id + "'";
+    pool.query(query, function(err, result) {
+        callback(result);
+    })
+}
+
+exports.getProductList = function(type, callback) {
+    query = "select * from \"products\" where loai = '" + type + "'";
+    pool.query(query, function(err, result) {
+        callback(result);
+    })
+}
+
+exports.getProductBrand = function(type, brand, callback){
+    query = "select * from \"products\" where brand = '" + brand + "' and loai = '" + type + "'";
+    pool.query(query, function(err, result) {
+        callback(result);
+    });
+}
+
+exports.getProductOrder  = function(type, brand, order, callback){
+    var query;
+    if (brand != 'undefined')
+        query = "select * from \"products\" where brand = '" + brand + "' and loai = '" + type + "' ORDER BY gia " + order + "";
+    else
+        query = "select * from \"products\" where loai = '" + type + "' ORDER BY gia " + order + "";
+    pool.query(query, function(err, result) {
+        callback(result);
+    });
+}
+
