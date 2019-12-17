5f32afc5655ca5550344d65c1ebe4bdaf3e2d394
tqdat99 <tqdat99@gmail.com>
2019-12-17 14:03:41 +0700

    commit backup

--- controllers/store.js
+++ controllers/store.js
@@ -4,7 +4,9 @@ module.exports.productListByType = function(req, res) {
     productModel.getProductListByType(req.params['type'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user
+            /*Type: req.params['type'],
+            Brand: 'undefined',
+            user: req.user*/
         });
     })
 }
@@ -13,7 +15,9 @@ module.exports.productListByBrand = function(req, res) {
     productModel.getProductListByBrand(req.params['brand'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user
+            /*Brand: req.params['brand'],
+            Type: 'undefined',
+            user: req.user*/
         });
     })
 }
@@ -32,29 +36,13 @@ module.exports.productDetailById = function(req, res) {
     })
 }
 
-module.exports.productListByTypeAndPrice = function(req, res) {
-    productModel.getProductTypeAndPrice(req.params['type'], req.params['price'], function(items) {
-        res.render('store', {
-            Items: items,
-            user: req.user,
-        });
-    })
-}
-
-module.exports.productListByBrandAndPrice = function(req, res) {
-    productModel.getProductBrandAndPrice(req.params['brand'], req.params['price'], function(items) {
-        res.render('store', {
-            Items: items,
-            user: req.user,
-        });
-    })
-}
-
 module.exports.productListByTypeAndOrder = function(req, res) {
     productModel.getProductTypeAndOrder(req.params['type'], req.params['order'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user,
+            /*Type: req.params['type'],
+            Order: req.params['order'],
+            user: req.user,*/
         });
     })
 }
@@ -63,7 +51,9 @@ module.exports.productListByBrandAndOrder = function(req, res) {
     productModel.getProductBrandAndOrder(req.params['brand'], req.params['order'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user,
+            /*Brand: req.params['brand'],
+            Order: req.params['order'],
+            user: req.user,*/
         });
     })
 }
@@ -72,7 +62,9 @@ module.exports.productListByTypeAndBrand = function(req, res) {
     productModel.getProductListByTypeAndBrand(req.params['type'], req.params['brand'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user
+            /*Type: req.params['type'],
+            Brand: req.params['brand'],
+            user: req.user*/
         });
     })
 }
@@ -81,44 +73,32 @@ module.exports.productListByTypeBrandOrder = function(req, res) {
     productModel.getProductTypeBrandOrder(req.params['type'], req.params['brand'], req.params['order'], function(items) {
         res.render('store', {
             Items: items,
-            user: req.user,
+            /*Brand: req.params['brand'],
+            Type: req.params['type'],
+            Order: req.params['order'],
+            user: req.user,*/
         });
     })
 }
 
-module.exports.productListByTypeBrandPrice = function(req, res) {
-    productModel.getProductTypeBrandPrice(req.params['type'], req.params['brand'], req.params['price'], function(items) {
-        res.render('store', {
-            Items: items,
-            user: req.user,
-        });
-    })
-}
-
-module.exports.productListByTypeOrderPrice = function(req, res) {
-    productModel.getProductTypeOrderPrice(req.params['type'], req.params['order'], req.params['price'], function(items) {
-        res.render('store', {
-            Items: items,
-            user: req.user,
-        });
-    })
-}
-
-module.exports.productListByBrandOrderPrice = function(req, res) {
-    productModel.getProductBrandOrderPrice(req.params['brand'], req.params['order'], req.params['price'], function(items) {
-        res.render('store', {
-            Items: items,
-            user: req.user,
-        });
-    })
-}
-
-module.exports.productListFilter = function(req, res) {
+/*module.exports.productListFilter = function(req, res) {
     productModel.getProductFilter(req.params['type'], req.params['brand'], req.params['price'], req.params['order'], function(items) {
-        console.log(items)
         res.render('store', {
             Items: items,
+            Type: req.params['type'],
+            Brand: req.params['brand'],
             user: req.user,
         });
     })
 }
+
+module.exports.productlistOrder = function(req, res) {
+    productModel.getProductOrder(req.params['type'], req.params['brand'], req.params['order'], function(items) {
+        res.render('store', {
+            Items: items,
+            Type: req.params['type'],
+            Brand: req.params['brand'],
+            user: req.user,
+        });
+    })
+}*/
