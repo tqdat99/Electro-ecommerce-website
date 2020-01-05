c82e32385b3d74a836511f54b57c80ef2532fe15
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-01-05 15:19:36 +0700

    Update route index

--- routes/index.js
+++ routes/index.js
@@ -8,17 +8,17 @@ router.get('/', index.home)
 
 /*router.get('/cart', function(req, res) {
     res.render('cart');
-});
+});*/
 
-router.get('/checkout', function(req, res) {
+/*router.get('/checkout', function(req, res) {
     res.render('checkout');
-});
+});*/
 
-router.get('/forget-password', function(req, res) {
+/*router.get('/forget-password', function(req, res) {
     res.render('forget-password');
-});
+});*/
 
-router.get('/account', function(req, res) {
+/*router.get('/account', function(req, res) {
     res.render('account');
 });
 
