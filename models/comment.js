ba80c61512b93dad006fb63e77f918cbcb44f99c
Thu Anh Nguyen <anhthu@Thus-MacBook-Air.local>
2020-01-07 12:48:55 +0700

    Fix comments

--- models/comment.js
+++ models/comment.js
@@ -2,13 +2,12 @@ var database = require('./database')
 var pool = database.pool;
 
 module.exports.insertComment = function(name, message, productid, time, callback) {
-    //console.log(time)
-    query = 'insert into "comments" (productid, username, content, name, date) values (\'' + productid + '\', \'' + message + '\' , \'' + name + '\', \'' + time + '\')';
+    query = 'insert into "comments" (productid, username, content, name, date) values (\'' + productid + '\', null, \'' + message + '\' , \'' + name + '\', \'' + time + '\')';
     pool.query(query, function(err, result) {
-        //console.log(query)
         callback(result);
     });
 }
+
 
 module.exports.getCommentsByProductId = function(id, callback) {
     query = "select * from \"comments\" where productid = '" + id + "' order by commentid desc";
